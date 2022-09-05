d02a5b6f431771668041770cefd32c1dcb24267f
Redfield-13 <fieldr62@gmail.com>
2022-09-05 09:59:52 +0200

    init,sign page done

---
+++ src/components/sign.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function sign() {
+  return (
+    <div className='sign'>
+        <div className="logo">Care-Zone</div>
+        <div className="wel">Welcome to Care-Zone health care center platform, please sign in or sign up to use the platform.</div>
+        <button className='signin'>Sign In</button>
+        <button className='signin'>Sign Up</button>
+    </div>
+  )
+}
+
+export default sign
